3aa33e279a6dfb408b2f82bbb6cd4b9223d2960e
baely <bailey.butler.234@gmail.com>
2021-12-09 09:23:44 +1100

    Update oneliner.py

--- 6.1/oneliner.py
+++ 6.1/oneliner.py
@@ -1,1 +1,6 @@
-f=[0]*9;[f:=f[:g]+[f[g]+1]+f[g+1:]for g in map(int,open("input.txt").read().split(","))];[(f:=f[1:]+f[:1],f.insert(6,f.pop(6)+f[7]))for _ in range(80)];print(sum(f))
+f=[0]*9;[f:=f[:h]+[f[h]+1]+f[h+1:]for g in open("input.txt").read()if(h:=ord(g)-48)>=0];[(f:=f[1:]+f[:1],f.insert(6,f.pop(6)+f[7]))for _ in[0]*80];print(sum(f))
+
+f=[0]*9
+for g in open("input.txt").read().split(","):f[int(g)]+=1
+for i in range(80):f[(i+7)%9]+=f[i%9]
+print(sum(f))
